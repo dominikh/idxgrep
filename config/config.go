9eed9faad61851ebd9b52e562c9c29fe82666621
Dominik Honnef <dominik@honnef.co>
2018-04-28 13:11:02 +0200

    config: add configuration

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/kirsle/configdir"
+	"github.com/naoina/toml"
+)
+
+var DefaultPath = filepath.Join(configdir.LocalConfig("idxgrep"), "idxgrep.conf")
+
+type Config struct {
+	Indexing Indexing `toml:"indexing"`
+}
+
+type Indexing struct {
+	MaxFilesize int `toml:"max_filesize"`
+}
+
+func Load(r io.Reader) (*Config, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	err = toml.Unmarshal(b, &cfg)
+	return &cfg, err
+}
+
+func LoadFile(path string) (*Config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return &Config{}, nil
+		}
+		return nil, err
+	}
+	defer f.Close()
+	return Load(f)
+}
