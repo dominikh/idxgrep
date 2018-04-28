c8b3d8ad2b5e0efa89de504347992b9ed2d2f84b
Dominik Honnef <dominik@honnef.co>
2018-04-28 13:49:41 +0200

    cmd/idxgrep: support case-insensitive search

--- cmd/idxgrep/idxgrep.go
+++ cmd/idxgrep/idxgrep.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
-	"regexp/syntax"
 
 	"honnef.co/go/idxgrep/es"
 	"honnef.co/go/idxgrep/internal/parser"
@@ -11,26 +11,31 @@ import (
 )
 
 func main() {
-	client := es.Client{
-		Base: "http://localhost:9200",
-	}
-
-	re, err := syntax.Parse(os.Args[1], syntax.Perl)
-	if err != nil {
-		panic(err)
-	}
-	q := parser.RegexpQuery(re)
-
-	hits := client.Search(q)
-
-	ree, _ := regexp.Compile(os.Args[1])
 	grep := regexp.Grep{
-		Regexp: ree,
 		Stdout: os.Stdout,
 		Stderr: os.Stderr,
 	}
 	grep.AddFlags()
+	insensitive := flag.Bool("i", false, "Case insensitive")
 	flag.Parse()
+
+	pat := "(?m)" + flag.Args()[0]
+	if *insensitive {
+		pat = "(?i)" + pat
+	}
+	re, err := regexp.Compile(pat)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't parse regexp:", err)
+	}
+	q := parser.RegexpQuery(re.Syntax)
+	grep.Regexp = re
+
+	client := es.Client{
+		Base: "http://localhost:9200",
+	}
+
+	hits := client.Search(q)
+
 	for _, hit := range hits {
 		name := hit.ID
 		f, err := os.Open(name[len("file://"):])
